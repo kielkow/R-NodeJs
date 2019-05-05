1482e6764679764a9362402dd42536da196cfd15
kielkow <matheuskiel@fiorifer.com.br>
2019-05-05 20:49:57 -0300

    Criando o DB

--- sever.js
+++ sever.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const requireDir = require('require-dir')
 
 //Inicando o app
 const app = express()
@@ -7,10 +8,20 @@ const app = express()
 //Iniciando com o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true })
 
+requireDir('./src/models')
+
+const Product = mongoose.model("Product")
+
 
 //Primeira rota
 app.get('/', (req, res) => {
-    res.send("Hello World")
+    Product.create({
+        title: "React Native",
+        description: "Build native apps with React",
+        url: "http://github.com/facebook/react-native"
+    })
+    
+    return res.send("Hello World")
 })
 
 app.listen(3001)
